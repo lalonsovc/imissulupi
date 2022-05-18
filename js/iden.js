7bde45883c85928a8ed27d0a570a23f0ec1d8e14
lalonsovc <81634204+lalonsovc@users.noreply.github.com>
2022-05-17 21:44:56 -0500

    v1

---
+++ js/iden.js
@@ -0,0 +1,7 @@
+var count = 4;
+        setInterval(function(){
+        count--;
+            if (count == 0) {
+                window.location = 'https://youtu.be/F_Te-qDpfFA'; 
+            }
+        },1000);
